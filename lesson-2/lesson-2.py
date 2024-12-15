077fa2c0f66f856da63f900d3eddff78b95f1c85
Nozima <quvondioqvanozimaxon011@gmail.com>
2024-12-15 18:32:05 +0500

    lesson2

---
+++ lesson-2/lesson-2.py
@@ -0,0 +1,15 @@
+#1
+  
+# a= 23.4566
+# print(f"{a}:.2f")
+
+
+#2
+
+a = int(input("birinchi sonni kiriting: a="))
+b = int(input("ikkinchi sonni kiriting: b="))
+c = int(input("uchinchi sonni kiriting: c="))
+if a>b>c:
+    print(a, c)6
+
+    
