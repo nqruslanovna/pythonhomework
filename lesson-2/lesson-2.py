37d52159ff4723842136755a283962232db9ae91
Nozima <quvondioqvanozimaxon011@gmail.com>
2024-12-16 15:20:25 +0500

    lesson-2(10x)

--- lesson-2/lesson-2.py
+++ lesson-2/lesson-2.py
@@ -1,15 +1,70 @@
 #1
   
-# a= 23.4566
-# print(f"{a}:.2f")
+# a= float(input("yaxlitlanadigan sonni kiriting: a= "))
+# print(f"{a:.2f}")
 
 
 #2
 
-a = int(input("birinchi sonni kiriting: a="))
-b = int(input("ikkinchi sonni kiriting: b="))
-c = int(input("uchinchi sonni kiriting: c="))
-if a>b>c:
-    print(a, c)6
+# a = int(input("birinchi sonni kiriting: a="))
+# b = int(input("ikkinchi sonni kiriting: b="))
+# c = int(input("uchinchi sonni kiriting: c="))
 
-    
+# min_number = min(a,b,c)
+# max_number = max(a,b,c)
+# print("eng kichigi:", min_number)
+# print("eng kattasi: ", max_number)
+
+
+
+#3
+# l = float(input("masofani kiriting: L(km)= "))
+# l_m = l*1000
+# l_cm = l*100000
+
+# print("Bu masofa santimetrlarda", l_cm, " cm ga va metrlarda", l_m, "m ga teng")
+
+#4
+# a = int(input('a='))
+# b = int(input("b="))
+# c=a//b 
+# d=a%b 
+# print(c,"dan yetib",d,"qoldiq qoldi")
+
+#5
+# t = float(input("Temperaturani kiriting: t(C)= "))
+# F=t*9/5+32
+# print(F,"Farangeyt")
+
+#6
+# n = input("Raqamni kiriting: n = ")
+# oxirgi_raqam = n[-1]
+# print("oxirgi raqam", oxirgi_raqam)
+
+#7
+# a = int(input("sonni kiriting: "))
+# print("Toq"*(a%2) + "Juft"*(1-a%2))
+
+#8
+# ism = input("Ismingizni kiriting: ")
+# yil = int(input("Tug'ilgan yilingizni kiriting: "))
+# yosh = 2024-yil
+# print("Hurmatli", ism, "siz",yosh,"yoshdasiz!")
+
+#9
+# txt = 'LMaasleitbtui'
+# car1 = txt[1] + txt[2] + txt[5] + txt[7] + txt[9] + txt[11]
+# car2 = txt[0] + txt[3] + txt[4] + txt[6] + txt[8] + txt[10] + txt[12]
+# print(car1)
+# print(car2)
+
+#10
+# word = input("ism familiyangizni kiriting: ")
+# print(len(word))
+# print(word.upper())
+# print(word.lower())
+# print(word.title())
+# print(word.capitalize())
+# print(word[::-1])
+
+
