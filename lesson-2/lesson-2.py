d005964d59720b8ce86349caa7764142693b8c96
Nozima <quvondioqvanozimaxon011@gmail.com>
2024-12-23 19:29:28 +0500

    homework

--- lesson-2/lesson-2.py
+++ lesson-2/lesson-2.py
@@ -68,3 +68,69 @@
 # print(word[::-1])
 
 
+# 11
+# print("Men olmani yaxshi ko'raman")
+# meva = input(" siz yoqtiradigan meva nomini kiriting:")
+# print('Men',meva+"larni yaxshi ko'raman")
+
+# 12
+# satr = input("Satrni kiriting:")
+# print("birinchi belgi:", satr[0])
+# print("oxirgi belgi:", satr[-1])
+
+
+# 13
+# satr = input("satrni kiriting:")
+# print(satr[::-1])
+
+
+# 14
+# nom = input("Foydalanuvchi nomini kiriting: ")
+# parol = input("parolni kiriting: ")
+# if nom!=0 and parol==0:
+#     print("parolni kiritishni unutdingiz!")
+# elif nom!=0 and parol!=0:
+#     print("sizning so'rovnomangiz qabul qilindi!")
+# else:
+#     print("Ma'lumotlarni toliq kiriting!")
+
+# 15
+# a = int(input("a="))
+# b = int(input("b="))
+# if a==b:
+#     print("raqamlar teng")
+# else:
+#     print("raqamlar teng emas")   
+
+
+# 16
+# a = int(input("a="))
+# if 1-a%2 and a>0:
+#     print("bu son juft va musbat")
+# else:
+#     print("biz xohlagan son emas")
+
+# 17
+# a = int(input("a="))
+# b = int(input("b="))
+# c = int(input("c="))
+# if a==b==c:
+#     print("uchala son teng")
+# else:
+#     print("bu sonlar teng emas")
+
+# 18
+# a = int(input("sonni kiriting:"))
+# if a%3==0 and a%5==0:
+#     print("bu son 3 ga va 5 ga bolinadi")
+# else:
+#     print("bu son 15 ga bolinmaydi")
+
+
+# 19
+# x = float(input("x="))
+# y = float(input("y= "))
+# if x+y>50:
+#     print('x+y>50')
+# else:
+#     print("x+y<50")
